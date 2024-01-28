b550ede3cbeb711b943b507462983c13a2718c83
k4deng <70725766+k4deng@users.noreply.github.com>
2024-01-27 19:57:31 -0500

    update default user pfp

--- src/models/user.js
+++ src/models/user.js
@@ -41,7 +41,7 @@ const userSchema = new Schema({
   photoUrl: {
     type: String,
     default:
-        'https://upload.wikimedia.org/wikipedia/commons/4/47/HI_all.png',
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png',
   },
 
   language: {
