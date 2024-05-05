bb0de3cd3d4696769792a0425e02b5e19182f2b7
k4deng <70725766+k4deng@users.noreply.github.com>
2024-05-05 16:00:35 -0400

    add decimal calculations to grades page

--- src/api/controllers/grades/class-gradespage.js
+++ src/api/controllers/grades/class-gradespage.js
@@ -9,6 +9,7 @@ export default async (req, res) => {
     return res.render('grades/class-gradespage.ejs', {
         site: client,
         user: req.session.user ? await User.findOne({ _id: req.session.user }).exec() : null,
+        classId: req.params.id,
         html: html
     });
 };
